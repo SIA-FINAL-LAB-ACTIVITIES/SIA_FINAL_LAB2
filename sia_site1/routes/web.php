1d73ee4d090c38f1c7ae91414eb86b8b827ceb10
Khen <paler.khen.ustp@gmail.com>
2023-06-22 07:33:24 +0800

    add

---
+++ sia_site1/routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+// unsecure routes
+/*$router->group(['prefix' => 'api'], function () use ($router){
+    $router->get('/users',['uses' => 'UserController@getUsers']);
+});*/
+
+
+//$router->get('/users',['uses' => 'UserController@getUsers']);
+
+
+//more simple routes
+$router->get('/users', 'UserController@index'); // get all users records
+$router->post('/users','UserController@add'); // create new user record
+$router->get('/users/{id}','UserController@show'); // get user by id
+
+
+$router->post('/add/users',['uses' => 'UserController@addUser']); //1
+
+$router->delete('/delete/users/{id}',['uses' => 'UserController@deleteUser']); //2
+
+$router->put('/update/users/{id}',['uses' => 'UserController@updateUser']); //3
+
+$router->get('/find/users/{id}',['uses' => 'UserController@showUser']); //4
+
+$router->get('/show/users',['uses' => 'UserController@showALLUSERS']); //5
